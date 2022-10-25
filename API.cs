2caae8d05b02894031b9dc67d933611b23531960
Joseph McGloin <s00212257@atu.ie>
2022-10-25 14:36:14 +0100

    API class added.

---
+++ API.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Helpful resources: https://stackoverflow.com/questions/9620278/how-do-i-make-calls-to-a-rest-api-using-c
+                     https://code-maze.com/different-ways-consume-restful-api-csharp/
+*/
+
+
+namespace Project300
+{
+    internal class API
+    {
+        public static void FetchAPIData()
+        {
+            HttpClient client = new HttpClient();
+
+        }
+    }
+}
