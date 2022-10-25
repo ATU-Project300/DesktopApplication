6b8445cd3ca5bec703be6b762331445e4b5025d2
Joseph McGloin <s00212257@atu.ie>
2022-10-25 14:24:51 +0100

    Emulator and Game classes created.

---
+++ Emulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project300
+{
+    internal class Emulator
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Stability StabilityRating { get; set; }
+        public string Image { get; set; }
+        public string Console { get; set; }
+
+        public enum Stability
+        {
+            Stable,
+            Unstable,
+            Experimental
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Project300.Emulator;
+
+namespace Project300
+{
+    internal class Game
+    {
+        public string Title { get; set; }
+        public int YearReleased { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string[] Consoles { get; set; }
+        public Emulator Emulator { get; set; }
+    }
+}
