fddf62c07ce0a54b097d3a4271a20693a7e5d4f6
s00212257 <s00212257@atu.ie>
2022-11-29 12:53:40 +0000

    Add API data fetch

--- API.cs
+++ API.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +10,29 @@ using System.Threading.Tasks;
                      https://code-maze.com/different-ways-consume-restful-api-csharp/
 */
 
-
 namespace Project300
 {
     internal class API
     {
         public static void FetchAPIData()
         {
-            HttpClient client = new HttpClient();
+            using HttpClient client = new();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET API Reader");
+
+            var GamesList = await ProcessDataAsync(client);
+
+            static async Task<List<GamesList>> ProcessDataAsync(HttpClient client)
+            {
+                await using Stream stream = await client.GetStreamAsync("http://localhost:3000/games");
+                var games = await JsonSerializer.DeserializeAsync<List<GamesList>>(stream);
+                return games ?? new();
+            }
 
         }
     }
