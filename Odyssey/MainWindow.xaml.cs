98ac5300657a14893087ca34450b0f3c85bb76d2
s00212257 <s00212257@atu.ie>
2022-11-30 16:32:23 +0000

    Add Dark Mode toggle

--- Odyssey/MainWindow.xaml.cs
+++ Odyssey/MainWindow.xaml.cs
@@ -2,14 +2,51 @@
 
 namespace Odyssey
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        // On application startup
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Load stored settings
+            loadSettings();
+        }
+
+        private void applyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            saveSettings();
+            MessageBox.Show("Settings applied");
+        }
+
+        //Load settings on startup
+        private void loadSettings()
+        {
+            if (!Properties.Settings.Default.DarkMode)
+            {
+                //Disable dark mode
+                darkModeStatus.Content = "Dark mode disabled";
+                darkModeChkBx.IsChecked = false;
+            }
+            else
+            {
+                //Dark mode is enabled
+                darkModeStatus.Content = "Dark mode enabled";
+                darkModeChkBx.IsChecked = true;
+            }
+        }
+
+        //Save settings
+        private void saveSettings()
+        {
+            //Save dark mode setting
+            Properties.Settings.Default.DarkMode = darkModeChkBx.IsChecked.GetValueOrDefault();
+
+            Properties.Settings.Default.Save();
         }
     }
 }
