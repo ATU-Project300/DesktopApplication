25ac028fc9850bd1835a2f0da9eb47375e3f59ab
s00212257 <s00212257@atu.ie>
2022-11-30 15:36:42 +0000

    New base

---
+++ Odyssey/Emulator.cs
@@ -0,0 +1,11 @@
+﻿namespace Odyssey
+{
+    internal class Emulator
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Console { get; set; }
+        public string ExecutableLocation { get; set; }
+    }
+}
---
+++ Odyssey/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Odyssey
+{
+    internal class Game
+    {
+        [property: JsonPropertyName("title")] public string Title { get; set; }
+        [property: JsonPropertyName("year")] public int YearReleased { get; set; }
+        [property: JsonPropertyName("description")] public string Description { get; set; }
+        [property: JsonPropertyName("image")] public string Image { get; set; }
+        [property: JsonPropertyName("console")] public string[] Consoles { get; set; }
+        [property: JsonPropertyName("emulator")] public Emulator Emulator { get; set; }
+        public string FileLocation { get; set; } //Location of the ROM/ISO, add this to the local INI file mentioned in MainWindow.xaml.cs
+    }
+}
