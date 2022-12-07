bec21b25445faec17a48357d342aa5a2a86b0c2f
s00212257 <s00212257@atu.ie>
2022-12-07 19:47:05 +0000

    Material Design added

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Documents;
 using static API.API;
 
 namespace WpfApp1
