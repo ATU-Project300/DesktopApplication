bc51e0d3077e0872d23329b800c36db51006ec6c
Joseph McGloin <S00212257@atu.ie>
2023-03-10 15:03:20 +0000

    Add EmulatorsList class

---
+++ API/EmulatorsList.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API;
+
+public sealed record EmulatorsList(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("uri")] int Uri,
+    [property: JsonPropertyName("image")] string Image);
